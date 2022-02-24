23142307af6dfad74bf6c2842039c901277df330
yonax73 <yonax73@gmail.com>
2022-02-24 10:22:09 -0500

    Async - await

---
+++ Async-await.js
@@ -0,0 +1,29 @@
+const urls = [
+
+    'https://jsonplaceholder.typicode.com/users',
+    'https://jsonplaceholder.typicode.com/posts',
+    'https://jsonplaceholder.typicode.com/albums',
+    
+]
+
+const getData = async function(){
+
+    try{
+
+        const [a, b, c, d] = await Promise.all(urls.map( url => 
+
+            fetch(url).then( resp => resp.json())
+    
+        ))
+        
+        console.log('users', a)
+        console.log('posts', b)
+        console.log('albums', c)
+    
+    }catch(err){
+
+        console.log('oops',err)
+        
+    }
+
+}
