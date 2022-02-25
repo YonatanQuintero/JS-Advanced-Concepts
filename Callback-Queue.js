706d7db629726d01ddccf28a5469bead7ca5a78c
yonax73 <yonax73@gmail.com>
2022-02-25 10:58:54 -0500

    Callback - Task queue

---
+++ Callback-Queue.js
@@ -0,0 +1,11 @@
+//Callback Queue - Task Queue
+setTimeout( () => { console.log('1','is the loneliest number')}, 0 )
+setTimeout( () => { console.log('2', 'can be as bad as one') }, 10)
+console.log('3', 'is a crowd')
+
+//Ouput
+// 3 is a crowd
+// 1 is the loneliest number
+// 2 can be as bad as one
+
+
